8367e4c1228f77ed9e07cad332961e55c3905ab6
Gabriel-Batista <gabedbatista@gmail.com>
2019-04-28 19:20:18 -0400

    Done.

--- password.js
+++ password.js
@@ -1,1 +1,22 @@
-// code solution here
+let generatePassword = (length) => {
+  let result = ""
+  for(let i=0; i < length; i++) {
+    result += String.fromCharCode(Math.random() * (126 - 33) + 33)
+  }
+  return result
+}
+
+let PasswordGenerator = () => {
+  if (process.argv.length !== 4) {
+    console.log("Incorrect number of arguments, please use this format: password <length> <Number Of>")
+    process.exit(1)
+  }
+  else  {
+    for(let i=0; i < process.argv[3]; i++) {
+      console.log(generatePassword(process.argv[2]))
+    }
+    process.exit(0)
+  }
+}
+
+PasswordGenerator()
